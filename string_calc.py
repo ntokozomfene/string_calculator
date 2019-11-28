d91651acc77415d5e606ec851a22648c9a9056b9
RulaniPanguana <filipe.panguana@outlook.com>
2019-11-28 10:56:26 +0200

    first commit

---
+++ string_calc.py
@@ -0,0 +1,27 @@
+import re
+
+def add(num):
+    num_list = []
+    neg_num = []
+
+    if len(num) == 0: return 0
+    
+    try:
+        int(num[-1])
+    except:
+        return "Not ok"
+       
+    for each_letter in re.findall(r"-?\d+", num):
+        try:
+            if int(each_letter) > 1000:
+                each_letter = 0
+            if int(each_letter) < 0:
+                neg_num.append(each_letter)
+            num_list.append(int(each_letter))
+        except:
+            continue
+            
+    if len(neg_num) > 0:
+        raise Exception('negatives not allowed: {}'.format(neg_num))
+
+    return sum(num_list)
